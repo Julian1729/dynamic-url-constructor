43f10cfb108c95f4ea90e76511629e77d0fd1543
Julian1729 <hernandez.julian17@gmail.com>
2019-05-02 13:34:04 -0400

    Add and test example

---
+++ example/routes.js
@@ -0,0 +1,28 @@
+/**
+ * Export Routes
+ */
+
+const URLConstructor = require('../urlconstructor');
+
+let ExampleURLConstructor = new URLConstructor();
+
+// Configure Constructor
+ExampleURLConstructor.setBase('http://examplesite.org');
+
+/**
+ * Define Routes
+ */
+
+// Dashboard Route
+let dashboard = ExampleURLConstructor.addRoute('home', '/:username/dashboard');
+
+// Account Settings Route
+let settings = ExampleURLConstructor.addRoute('account-settings', '/:username/settings');
+
+// Age Calculator
+let ageCalculator = ExampleURLConstructor.addRoute('age-Calculator', '/age-calculator/:dob');
+
+// Logout Route
+let logout = ExampleURLConstructor.addRoute('logout', '/?logout=1');
+
+module.exports = {dashboard, settings, ageCalculator, logout};
---
+++ tests/example.spec.js
@@ -0,0 +1,31 @@
+/**
+ * Test Examples
+ */
+const {expect} = require('chai');
+
+describe('example/routes.js', () => {
+
+  let routes = require('../example/routes');
+
+  // Example User Variables
+  const user = {
+    name: 'Michael Scott',
+    dob: '1962-04-15',
+    username: 'littlekidlover'
+  }
+
+  it('should export routes', () => {
+
+    expect(routes).to.have.property('dashboard');
+    expect(routes.dashboard.url).to.be.a('function');
+
+  });
+
+  it('should render correct URLs', () => {
+
+    routes.dashboard.setParam('username', user.username);
+    expect(routes.dashboard.url({})).to.equal('http://examplesite.org/littlekidlover/dashboard');
+
+  });
+
+});
